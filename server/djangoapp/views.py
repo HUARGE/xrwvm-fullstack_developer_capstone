e285cbd468df85245c9559cbfe1f65ad61a14260
Hüseyin Arda <senin-email@example.com>
2026-09-09 09:28:06 -0400

    Add dealership views and review sentiment analysis

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -94,6 +94,72 @@ def registration(request):
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
+
+# Get all dealerships or dealerships by state
+def get_dealerships(request, state="All"):
+    if state == "All":
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/" + state
+
+    dealerships = get_request(endpoint)
+
+    return JsonResponse({
+        "status": 200,
+        "dealers": dealerships
+    })
+
+
+# Get dealer details
+def get_dealer_details(request, dealer_id):
+    if dealer_id:
+        endpoint = "/fetchDealer/" + str(dealer_id)
+        dealership = get_request(endpoint)
+
+        return JsonResponse({
+            "status": 200,
+            "dealer": dealership
+        })
+    else:
+        return JsonResponse({
+            "status": 400,
+            "message": "Bad Request"
+        })
+
+
+# Get dealer reviews and analyze sentiments
+def get_dealer_reviews(request, dealer_id):
+    if dealer_id:
+        endpoint = "/fetchReviews/dealer/" + str(dealer_id)
+        reviews = get_request(endpoint)
+
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            review_detail['sentiment'] = response['sentiment']
+
+        return JsonResponse({
+            "status": 200,
+            "reviews": reviews
+        })
+    else:
+        return JsonResponse({
+            "status": 400,
+            "message": "Bad Request"
+        })
+
+
+def add_review(request):
+    if(request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
+    else:
+        return JsonResponse({"status":403,"message":"Unauthorized"})
+
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
 # def get_dealerships(request):
